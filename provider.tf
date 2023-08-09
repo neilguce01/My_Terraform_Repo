c62f2eb14cffb0e1b03d8d1ec5d24e7677e62775
neilguce01 <iyangguce@gmail.com>
2023-08-09 18:01:46 +0800

    state file moved to s3

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   # Configuration options
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "s3statebackendneil123"
+    key            = "global/mystatefile/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
